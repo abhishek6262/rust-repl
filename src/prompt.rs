8977bd45e493e1953b1d4f336ddfec665ac7e808
Abhishek Prakash <abhishek.prakash@flexiloans.com>
2024-11-29 03:19:29 +0530

    add inquire

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,15 +1,9 @@
-use std::io::Write;
+use inquire::Text;
 
 pub struct Prompt;
 
 impl Prompt {
     pub fn input() -> String {
-        print!("> ");
-        std::io::stdout().flush().unwrap();
-
-        let mut buffer = String::new();
-        std::io::stdin().read_line(&mut buffer).unwrap();
-
-        buffer.trim_end().to_string()
+        Text::new(":").prompt().unwrap().to_string()
     }
 }
